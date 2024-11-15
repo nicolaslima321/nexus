0af8df3e3ab1e26016095c864d74b4c0f00f0496
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2024-11-15 11:53:12 -0300

    feat (wip): initiated table component

--- frontend/src/app/survivors/page.tsx
+++ frontend/src/app/survivors/page.tsx
@@ -1,3 +1,24 @@
+import Table from "~/components/common/Table";
+
 export default function SurvivorsPage() {
-  return <>Survivors Page</>;
+  const survivor = { name: 'nicolas', infected: true, iventory: ['watter'] };
+  const survivors = new Array({ length: 30 }).fill(survivor);
+
+  const tableHeaders = [
+    'lorem',
+    'ipsum',
+    'dolor',
+    'amet',
+  ];
+
+  const tableItems = new Array({ length: 30 }).fill([
+    'lorem ipsum dolor sit amet',
+    'ipsum',
+    'dolor',
+    'amet',
+  ]);
+
+  return (
+    <Table tableHeaders={tableHeaders} tableItems={tableItems} />
+  );
 }
