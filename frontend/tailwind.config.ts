71162ed8e880c0de32d90fec8fe07401ec34c3a5
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2024-11-15 11:22:30 -0300

    style: created dark theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
