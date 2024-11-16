828bedb5d455a28562683ee19ff731a376678609
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2024-11-16 16:35:30 -0300

    feat: created api handlers on nextjs

---
+++ frontend/src/app/api/login/route.ts
@@ -0,0 +1,31 @@
+import { performLogin } from '~/apis/account-api';
+
+import { cookies } from 'next/headers'
+
+export async function POST(request: Request) {
+  try {
+    const survivorLogin = await request.json();
+
+    const { accessToken, message } = await performLogin(survivorLogin);
+
+    const cookieStore = await cookies()
+
+    cookieStore.set('accessToken', accessToken);
+
+    return Response.json({ message }, { status: 200  });
+  } catch (error) {
+    if ([404, 401].includes(error?.status)) {
+      return Response.json(
+        { error: 'Login or password is invalid' },
+        { status: error.status }
+      );
+    }
+
+    console.error('Failed to perform login', error);
+
+    return Response.json(
+      { error: 'An error ocurred while performing login' },
+      { status: 500 }
+    );
+  }
+};
