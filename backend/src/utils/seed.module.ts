7182fe32acb6ede623391f123b1390a16af7c35d
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2024-11-17 23:14:14 -0300

    refactor: improved seed service

--- backend/src/utils/seed.module.ts
+++ backend/src/utils/seed.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeedService } from './seed.service';
 import { Item } from 'src/entities/item.entity';
+import { Survivor } from 'src/entities/survivor.entity';
+import { Inventory } from 'src/entities/inventory.entity';
+import { InventoryItem } from 'src/entities/inventory-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item])],
+  imports: [TypeOrmModule.forFeature([Item, Survivor, Inventory, InventoryItem])],
   providers: [SeedService],
   exports: [SeedService],
 })
--- backend/src/utils/seed.service.ts
+++ backend/src/utils/seed.service.ts
@@ -1,16 +1,39 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from 'src/entities/item.entity';
 import { Repository } from 'typeorm';
+import { faker } from '@faker-js/faker';
+import { Survivor } from 'src/entities/survivor.entity';
+import { Inventory } from 'src/entities/inventory.entity';
+import { InventoryItem } from 'src/entities/inventory-item.entity';
 
 @Injectable()
 export class SeedService implements OnModuleInit {
+  private readonly logger = new Logger(SeedService.name);
+
   constructor(
     @InjectRepository(Item)
     private itemRepository: Repository<Item>,
+    @InjectRepository(Survivor)
+    private survivorRepository: Repository<Survivor>,
+    @InjectRepository(Inventory)
+    private inventoryRepository: Repository<Inventory>,
+    @InjectRepository(InventoryItem)
+    private inventoryItemRepository: Repository<InventoryItem>,
   ) {}
 
-  async seedItems() {
+  async onModuleInit() {
+    this.logger.log('onModuleInit: starting seed process..');
+    await this.seedItems();
+
+    if (process.env.NODE_ENV !== 'development') {
+      await this.seedSurvivors();
+    } else {
+      this.logger.log('onModuleInit: productive environment, skiping remaining seeds..');
+    }
+  }
+
+  private async seedItems() {
     const items = [
       { id: 1, description: 'Water', name: 'Water', },
       { id: 2, description: 'Food', name: 'Food', },
@@ -25,13 +48,70 @@ export class SeedService implements OnModuleInit {
         const newItem = await this.itemRepository.create(item);
         await this.itemRepository.save(newItem);
       }
-      console.log('Seed data inserted!');
+      this.logger.log('seedItems: Items data inserted!');
     } else {
-      console.log('Items already exist, skipping seed.');
+      this.logger.log('seedItems: Items already exist, skipping seed.');
     }
   }
 
-  async onModuleInit() {
-    await this.seedItems();
+  private async seedSurvivors() {
+    const recordsToCreate = 100;
+
+    const existingSurvivors = await this.survivorRepository.find();
+
+    if (existingSurvivors.length < 100) {
+      for (let i = 0; i <= recordsToCreate; i++) {
+        const randomItems = await this.itemRepository.find();
+
+        const [latitude, longitude] = faker.location.nearbyGPSCoordinate();
+
+        const lastLocation = { latitude, longitude };
+
+        const survivor = this.survivorRepository.create({
+          // @ts-expect-error
+          gender: faker.datatype.boolean() ? 'male' : 'female',
+          infected: faker.datatype.boolean(),
+          name: faker.person.fullName(),
+          age: faker.number.int({ min: 18, max: 70 }),
+          createdAt: faker.date.recent({ days: 100 }),
+          lastLocation: lastLocation,
+        });
+
+        await this.survivorRepository.save(survivor);
+
+        const inventory = this.inventoryRepository.create({
+          survivor,
+        });
+
+        survivor.inventory = inventory;
+
+        await this.inventoryRepository.save(inventory);
+
+        await this.survivorRepository.save(survivor);
+
+        const hasItems = faker.datatype.boolean();
+
+        if (hasItems) {
+          const itemsCount = faker.number.int({ min: 1, max: 5 });
+
+          for (let i = 0; i < itemsCount; i++) {
+            const quantity = faker.number.int({ min: 1, max: 10 });
+            const randomIndex = faker.number.int({ min: 0, max: 3 });
+
+            const invItem = this.inventoryItemRepository.create({
+              inventory,
+              item: randomItems[randomIndex],
+              quantity,
+            });
+
+            this.inventoryItemRepository.save(invItem);
+          }
+        }
+      }
+
+      this.logger.log('seedSurvivors: Items data inserted!');
+    } else {
+      this.logger.log('seedSurvivors: Survivors already exist, skipping seed.');
+    }
   }
 }
